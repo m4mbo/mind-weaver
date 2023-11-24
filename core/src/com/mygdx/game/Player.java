c7326381a16d4f6beb793d5bf8d556d6a82ada79
Matias Barandiaran <115642529+m4mbo@users.noreply.github.com>
2023-11-24 01:52:16 +0100

    General outline of player and entity classes

---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+public class Player extends Entity {
+
+    private float xVelocity;
+    private float yVelocity;
+
+    public Player(int x, int y) {
+        super(x, y);
+    }
+
+    public void jump() {
+
+    }
+
+    public void attack() {
+
+    }
+
+    public void glide() {
+
+    }
+
+    public void grab() {
+
+    }
+}
