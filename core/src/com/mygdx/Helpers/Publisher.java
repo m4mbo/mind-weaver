638d51a7bde5742363fcd5f6fe22bfbafe608745
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-03-01 15:55:54 +0100

    Publisher class commenting

--- core/src/com/mygdx/Helpers/Publisher.java
+++ core/src/com/mygdx/Helpers/Publisher.java
@@ -1,6 +1,6 @@
 package com.mygdx.Helpers;
 
-public interface Publisher {    //Used to notify messages to every class that subscribes to this class
+public interface Publisher {    //Used to notify messages to every Subscriber that subscribes to this class
     public void addSubscriber(Subscriber subscriber);                   //Used to add a new subscriber
     public void removeSubscriber(Subscriber subscriber);                //Used to remove a specific subscriber
     public void notifyAllSubscribers(String flag);                      //Used to notify all subscribers
