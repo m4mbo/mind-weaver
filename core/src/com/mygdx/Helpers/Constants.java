eb2cda1dd745100b9cb0b835f7de54d827f0b0d4
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-03-02 17:50:23 +0100

    Constants class commenting

--- core/src/com/mygdx/Helpers/Constants.java
+++ core/src/com/mygdx/Helpers/Constants.java
@@ -1,23 +1,24 @@
 package com.mygdx.Helpers;
 
 public class Constants {
-    public static final int TILE_SIZE = 10;
-    public static final float PPM = 100;
+    public static final int TILE_SIZE = 10; //Used to maintain constant dimension regardless of machine size
+
+    public static final float PPM = 100;    //Pixels per meter
     public static final float MAX_SPEED_X = 1.2f;
     public static final float MAX_SPEED_Y = 5f;
-    public static final float TEXT_SPEED = 20;
-    public static final float KNOCKBACK_SCALE = 1f;
-    public static final float G = 11;
-    public static final float G_ENHANCED = 13;
-    public static final float BUTTON_WIDTH = 250;
-    public static final float BUTTON_HEIGHT = 175.5f;
+    public static final float TEXT_SPEED = 20;          //Speed at which text prints in cutscene
+    public static final float KNOCKBACK_SCALE = 1f;     //Move character backwards when attacked
+    public static final float G = 11;                   //Normal gravity for world
+    public static final float G_ENHANCED = 13;          //Enhanced gravity for falling in the world
+    public static final float BUTTON_WIDTH = 250;       //Button width
+    public static final float BUTTON_HEIGHT = 175.5f;   //Button height
 
     // Movement state Flag
     public enum MSTATE {
-        LEFT, RIGHT,
-        PREV, // Keep previous direction
-        HSTILL, // Horizontal still
-        FSTILL; // Full still
+        LEFT, RIGHT,    //Left movement, right movement
+        PREV,           // Keep previous direction
+        HSTILL,         // Horizontal still
+        FSTILL;         // Full still
     }
 
     // Player state flag
@@ -28,12 +29,12 @@ public class Constants {
     public enum ASTATE {
         RUN, JUMP, IDLE, FALL, LAND, OPEN, CLOSED, ATTACK, DEATH
     }
-
+    //Screen change operations to perform
     public enum SCREEN_OP {
         START, RESUME, LEVELS, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, MENU, EXIT, RESTART, CONTROLS
 
     }
-
+    //Slide transition directions
     public enum SLIDE_DIR {
         SLIDE_UP, SLIDE_DOWN, SLIDE_RIGHT, SLIDE_LEFT
     }
