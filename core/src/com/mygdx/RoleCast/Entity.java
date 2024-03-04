e66a98ace5214c2664e8729a2d43e95485f3fecc
Parichay Sachdev <parichay.sachdev04@outlook.com>
2024-03-04 11:08:34 +0100

    Entity class commenting

--- core/src/com/mygdx/RoleCast/Entity.java
+++ core/src/com/mygdx/RoleCast/Entity.java
@@ -3,13 +3,13 @@ package com.mygdx.RoleCast;
 import com.mygdx.Tools.MyResourceManager;
 import com.mygdx.Sprites.B2Sprite;
 
-// Every entity has a sprite attached to it
+//Every entity has a sprite attached to it
 public abstract class Entity extends B2Sprite {
     protected int ID;
     protected final MyResourceManager resourceManager;
     protected int lives;
 
-
+    //Instantiate Entity with ID and access to resource manager
     protected Entity(int ID, MyResourceManager resourceManager) {
         this.ID = ID;
         this.resourceManager = resourceManager;
@@ -23,5 +23,5 @@ public abstract class Entity extends B2Sprite {
         return lives;
     }
 
-    public void die() {}
+    public void die() {}    //Method to override for player
 }
